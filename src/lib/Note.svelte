13da050876ab211b7c55da4a0050aaab318acd01
nothendev <borodinov.ilya@gmail.com>
2023-05-08 22:00:37 +0300

    note.

--- src/lib/Note.svelte
+++ src/lib/Note.svelte
@@ -3,5 +3,10 @@ export let title: string;
 </script>
 
 <div class='my-8 flex rounded-3xl p-6 bg-sky-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10'>
-  <div>Note</div>
+  <div class="ml-4 flex-auto">
+    <p class='m-0 font-sans text-xl text-sky-900 dark:text-sky-400'>{title}</p>
+  <div class="prose mt-2.5 text-sky-800 [--tw-prose-background:theme(colors.sky.50)] prose-a:text-sky-900 prose-code:text-sky-900 dark:text-slate-300 dark:prose-code:text-slate-300">
+    <slot />
+  </div>
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
+<script>
+    import Note from "$lib/Note.svelte";
+</script>
 <div class="g-prose p-4 flex flex-col flex-1 items-center">
   <h1>netherite.chat</h1>
   <h2>The Discord that cares about its community.</h2>
 
   <hr />
 
-  
+  <Note title="Note">
+    Currently you have to pay a fee to create servers.
+    <br />
+    This requirement will be lifted once the project
+    is not losing money.
+  </Note>
 </div>
